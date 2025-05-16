d16f4afc826a5b6e04a53c2bf2e2c6381d78114e
Ethan Jappie <ethan.jappie@gmail.com>
2025-05-16 11:30:18 +0200

    initial commit

---
+++ iidentifii/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using iidentifii.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace iidentifii.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
